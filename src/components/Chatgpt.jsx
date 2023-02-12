bf42338676dec8cce1e109c963fa7fa111e95e39
aryankirsali <aryankirsali@gmail.com>
2023-02-12 16:08:12 +0530

    changed name of assistance bot

--- src/components/Chatgpt.jsx
+++ src/components/Chatgpt.jsx
@@ -7,7 +7,7 @@ const Chatgpt = () => {
   return (
     <div className="chatgpt">
       <div className="chatInfo">
-        <span>Chat GPT</span>
+        <span>Virtual Assistant</span>
       </div>
       <MessagesChatgpt />
       <InputChatgpt/>
